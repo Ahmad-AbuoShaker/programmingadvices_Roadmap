e303b80fcaee851b23d2ee7f2f74644ab2a50d8a
Ahmad AbuoShaker <ahmadabuoshaker@gmail.com>
2026-04-09 18:29:59 +0300

    Add files via upload

---
+++ 10- OOP as it Should Be/CalculaterProject.cpp
@@ -0,0 +1,96 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class clsCalculater {
+private:
+
+    float _LastNumber = 0;
+    float _PrevesResult = 0;
+    string _LastOperation = "Clear";
+    float _Resulte = 0;
+
+
+    bool _isZero(float Number) {
+        return Number == 0;
+    }
+
+public:
+
+    void Clear() {
+        _LastNumber = 0;
+        _PrevesResult = 0;
+        _LastOperation = "Clear";
+        _Resulte = 0;
+    }
+
+    void Add(float Number) {
+        _LastNumber = Number;
+        _PrevesResult = _Resulte;
+        _LastOperation = "Adding";
+        _Resulte += _LastNumber;
+    }
+
+    void Subtract(float Number) {
+
+        _LastNumber = Number;
+        _PrevesResult = _Resulte;
+        _LastOperation = "Subtracting";
+        _Resulte -= _LastNumber;
+    }
+
+    void Divide(float Number) {
+
+        _LastNumber = Number;
+        if (_isZero(Number)) {
+            Number = 1;
+        }
+
+        _PrevesResult = _Resulte;
+        _LastOperation = "Dividing";
+        _Resulte /= _LastNumber;
+    }
+
+    void Multiply(int Number) {
+
+        _LastNumber = Number;
+        _PrevesResult = _Resulte;
+        _LastOperation = "Multiplying";
+        _Resulte *= _LastNumber;
+    }
+
+    float GetLastResulte() {
+        return _Resulte;
+    }
+
+    void CancelLastOperation() {
+        _Resulte = _PrevesResult;
+        _LastNumber = 0;
+        _LastOperation = "Canceling Last Operation";
+
+    }
+
+
+    void PrintResult() {
+        cout << "\n Reulte";
+        cout<<" After " << _LastOperation << " " << _LastNumber << " is :" << _Resulte;
+
+    }
+
+
+};
+
+
+
+int main()
+{
+    clsCalculater Calculater;
+
+    Calculater.Add(5);
+    Calculater.PrintResult();
+
+    Calculater.Multiply(6);
+    Calculater.PrintResult();
+
+}
---
+++ 10- OOP as it Should Be/clsPersonProject.cpp
@@ -0,0 +1,97 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class clsPerson {
+private:
+
+    int _ID = 0;
+    string _FirstName;
+    string _LastName;
+    string _Email;
+    string _Phone;
+
+public:
+
+    clsPerson(string FirstName, string LastName, string Email, string Phone, int ID = 0) {
+        _ID = ID;
+        _FirstName = FirstName;
+        _LastName = LastName;
+        _Email = Email;
+        _Phone = Phone;
+    }
+
+    void setEmail(string Email) {
+        _Email = Email;
+    }
+
+    void setFirstName(string FirstName) {
+        _FirstName = FirstName;
+    }
+
+    void setLastName(string LastName) {
+        _LastName = LastName;
+    }
+
+    void setPhone(string Phone) { 
+        _Phone = Phone;
+    }
+
+
+    int ID(){
+        return _ID;
+    }
+
+    string FirstName() {
+        return _FirstName;
+    }
+
+    string LastName() {
+        return _LastName;
+    }
+
+    string Email() {
+        return _Email;
+    }
+
+    string Phone() {
+        return _Phone;
+    }
+
+    void Print() {
+        cout << "\nInfo:";
+        cout << "\n_____________________________";
+        cout << "\nID         : " << _ID;
+        cout << "\nFirst Name : " << _FirstName;
+        cout << "\nLast Name  : " << _LastName;
+        cout << "\nEmail      : " << _Email;
+        cout << "\nPhone      : " << _Phone;
+        cout << "\n_____________________________\n";
+    }
+
+    void SendSMS(string TextMessage) {
+        cout << "\nThe following SMS sent successfuly to phone : " << _Phone;
+        cout << "\n" << TextMessage << endl;
+    }
+
+    void SendEmail(string Subject,string Body) {
+        cout << "\nThe following message sent successfuly to email : " << _Email;
+        cout << "\nSubject : " << Subject;
+        cout << "\nBody : " << Body << endl;
+    }
+
+
+};
+    
+int main()
+{
+    clsPerson Person( "Ahmad", "Shaker", "Ahamdshh@gmail.com", "0956872241",18);
+    Person.Print();
+
+    Person.SendSMS("How are you ?");
+    Person.SendEmail("Hi", "Where are you ?");
+
+    system("pause>0");
+}
+
